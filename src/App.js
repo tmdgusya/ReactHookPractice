a3ffa210e8540cd570a46669987b7a5ac9b7a0b4
jsh <tmdgusya@naver.com>
2021-05-12 22:28:43 +0900

    feat : Component Class 를 통한 useState Hook 을 통한 상태값 증감 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import './App.css';
 import React, { useState } from "react";
 
-const App = () => {
+// const App = () => {
+//
+//   const [item, setItem] = useState(1);
+//   const incrementItem = () => setItem(item + 1);
+//   const decrementItem = () => setItem(item - 1);
+//
+//   return (
+//     <div className="App">
+//       <h1>Hello {item}</h1>
+//       <button onClick={incrementItem}>increment</button>
+//       <button onClick={decrementItem}>decrement</button>
+//     </div>
+//   );
+// }
 
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+class App extends React.Component {
 
-  return (
-    <div className="App">
-      <h1>Hello {item}</h1>
-      <button onClick={incrementItem}>increment</button>
-      <button onClick={decrementItem}>decrement</button>
-    </div>
-  );
+    constructor(props) {
+        super(props);
+        this.state = {
+            item : 1
+        }
+    }
+
+    incrementItem = () => {
+        this.setState(state => {
+            return {
+                item : this.state.item + 1
+            }
+        })
+    };
+
+    decrementItem = () => {
+        this.setState(state => {
+            return {
+                item : this.state.item - 1
+            }
+        })
+    };
+
+    render() {
+        const item = this.state.item;
+
+        return (
+            <div className="App">
+                <h1>Hello {item}</h1>
+                <button onClick={this.incrementItem}>increment</button>
+                <button onClick={this.decrementItem}>decrement</button>
+            </div>
+        )
+    }
+
 }
 
 export default App;
