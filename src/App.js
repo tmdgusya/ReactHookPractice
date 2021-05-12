e2b80c9ad3929ce446ded2c337422a3f67a88ec8
jsh <tmdgusya@naver.com>
2021-05-12 22:22:07 +0900

    feat : useState Hook 을 통한 상태값 증감 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
 
-function App() {
+const App = () => {
 
   const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {item}</h1>
+      <button onClick={incrementItem}>increment</button>
+      <button onClick={decrementItem}>decrement</button>
     </div>
   );
 }
