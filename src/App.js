4a436e03bff3fbcc348790aa1763ba6fa9539bcb
jsh <tmdgusya@naver.com>
2021-05-12 23:13:11 +0900

    feat: PageNation 추가

--- src/App.js
+++ src/App.js
@@ -1,58 +1,54 @@
 import './App.css';
 import React, { useState } from "react";
+import styled from 'styled-components';
 
-// const App = () => {
-//
-//   const [item, setItem] = useState(1);
-//   const incrementItem = () => setItem(item + 1);
-//   const decrementItem = () => setItem(item - 1);
-//
-//   return (
-//     <div className="App">
-//       <h1>Hello {item}</h1>
-//       <button onClick={incrementItem}>increment</button>
-//       <button onClick={decrementItem}>decrement</button>
-//     </div>
-//   );
-// }
+const App = () => {
 
-class App extends React.Component {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            item : 1
-        }
-    }
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <button onClick={incrementItem}>increment</button>
+      <button onClick={decrementItem}>decrement</button>'
+        <Page />
+    </div>
+  );
+}
 
-    incrementItem = () => {
-        this.setState(state => {
-            return {
-                item : this.state.item + 1
-            }
-        })
-    };
+const PageBar = styled.ul`
+    list-style: none;
+    padding-left: 30px;
+`
 
-    decrementItem = () => {
-        this.setState(state => {
-            return {
-                item : this.state.item - 1
-            }
-        })
-    };
+const PageNumber = styled.li`
+    float: left;
+    margin-left: 10px;
+`
 
-    render() {
-        const item = this.state.item;
 
-        return (
-            <div className="App">
-                <h1>Hello {item}</h1>
-                <button onClick={this.incrementItem}>increment</button>
-                <button onClick={this.decrementItem}>decrement</button>
-            </div>
-        )
-    }
+const Page = () => {
+    const [bar, setBar] = useState([1,2,3,4,5]);
 
+    const pageUp = () => setBar(
+        bar.map(page => page + 1)
+    )
+
+    const pageDown = () => setBar(
+        bar.map(page => page - 1)
+    )
+
+    return (
+        <div className="Page">
+            <PageBar key={bar.toString()}>
+                <PageNumber><button onClick={pageDown}>{'<<'}</button></PageNumber>
+                <PageNumber>{bar}</PageNumber>
+                <PageNumber><button onClick={pageUp}>{'>>'}</button></PageNumber>
+            </PageBar>
+        </div>
+    )
 }
 
 export default App;
